bd365ea6abf7ff4e5aa6d672c1560969a5b8fef5
hurben <hurben@bioinfo@gmail.com>
2021-04-22 16:20:46 -0500

    finish

--- src/LOO_ML.py
+++ src/LOO_ML.py
@@ -12,21 +12,21 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 
 def count_for_confusion_matrix(y_test, y_pred, count_1, count_2, count_3, count_4):
-    y_test = int(y_test)
-    y_pred = int(y_pred)
-    
-    if y_test == 1:    #obs MCII+
-        if y_pred == 1:  #pred MCII+
-            count_3 += 1
-        else:            #pred MCII-
-            count_4 += 1
-        
-    if y_test == 0:     #obs MCII-
-        if y_pred == 1:   #pred MCII+
-            count_1 += 1
-        else:             #pred MCII-
-            count_2 += 1
-    return count_1, count_2, count_3, count_4
+	y_test = int(y_test)
+	y_pred = int(y_pred)
+	
+	if y_test == 1:    #obs MCII+
+		if y_pred == 1:  #pred MCII+
+			count_3 += 1
+		else:            #pred MCII-
+			count_4 += 1
+		
+	if y_test == 0:     #obs MCII-
+		if y_pred == 1:   #pred MCII+
+			count_1 += 1
+		else:             #pred MCII-
+			count_2 += 1
+	return count_1, count_2, count_3, count_4
 
 
 def make_confusion_matrix(count_1, count_2, count_3, count_4):
@@ -34,51 +34,56 @@ def make_confusion_matrix(count_1, count_2, count_3, count_4):
 #observed MCII-  count 1, count 2
 #observed MCII+  count 3, count 4
 
-    tp =  count_2 / (count_2 + count_1)
-    tn =  count_3 / (count_3 + count_4)
-    acc = (count_3 + count_2) / (count_1 + count_2 + count_3 + count_4)
-    bal_acc = (tp + tn) / 2
+#note: technically, predicting MCII+ should be TP, but I am doing this for the consistency with Figure 5
 
-    print ('       #PRED_MCII+   #PRED_MCII-')
-    print ('obs_MCII-    %s            %s' % (count_1, count_2))
-    print ('obs_MCII+    %s            %s' % (count_3, count_4))
-    print ('TP: %s  (%s/%s)' % (tp, count_2, count_2 + count_1))
-    print ('TN: %s (%s/%s)' % (tn, count_3, count_3 + count_4))
-    print ('ACC: %s (%s/%s)' % (acc, count_3 + count_2,count_1 + count_2 + count_3 + count_4))
-    print ('bal ACC: %s (%s/%s)' % (bal_acc, tp + tn, 2))
+	tp =  count_2 / (count_2 + count_1)
+	tn =  count_3 / (count_3 + count_4)
+	acc = (count_3 + count_2) / (count_1 + count_2 + count_3 + count_4)
+	bal_acc = (tp + tn) / 2
+
+	print ('       #PRED_MCII+   #PRED_MCII-')
+	print ('obs_MCII-    %s            %s' % (count_1, count_2))
+	print ('obs_MCII+    %s            %s' % (count_3, count_4))
+	print ('TP: %s  (%s/%s)' % (tp, count_2, count_2 + count_1))
+	print ('TN: %s (%s/%s)' % (tn, count_3, count_3 + count_4))
+	print ('ACC: %s (%s/%s)' % (acc, count_3 + count_2,count_1 + count_2 + count_3 + count_4))
+	print ('bal ACC: %s (%s/%s)' % (bal_acc, tp + tn, 2))
 
 def main(data_df, specified_classifier):
-    r, c = data_df.shape
-    diff_count = 0
-    count_1 = 0
-    count_2 = 0
-    count_3 = 0
-    count_4 = 0
+	r, c = data_df.shape
+	diff_count = 0
+	count_1 = 0
+	count_2 = 0
+	count_3 = 0
+	count_4 = 0
 
-    for i in range(r):
+	for i in range(r):
 #         print ('%s/%s' % (i,r))
-        y_test = data_df.iloc[i,0]
-        X_test = data_df.iloc[i,1:]
+		y_test = data_df.iloc[i,0]
+		X_test = data_df.iloc[i,1:]
 
-        train_df = data_df.copy()
-        train_df.drop(index=train_df.iloc[i].name, inplace=True)
+		train_df = data_df.copy()
+		train_df.drop(index=train_df.iloc[i].name, inplace=True)
 
-        y_train = train_df.iloc[:,0]
-        X_train = train_df.iloc[:,1:]
+		#debug purpose
+		#train_df.to_csv('/Users/m221138/RA_microbiome/ml_data/train_df_%s.tsv' % i, sep="\t")
 
-        clf = specified_classifier
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict([X_test])
+		y_train = train_df.iloc[:,0]
+		X_train = train_df.iloc[:,1:]
 
-        if int(y_test) != int(y_pred):
-            diff_count += 1
+		clf = specified_classifier
+		clf.fit(X_train, y_train)
+		y_pred = clf.predict([X_test])
+
+		if int(y_test) != int(y_pred):
+			diff_count += 1
 #         print ("Answer: %s, Predicted: %s" % (y_test, y_pred))
 
-        count_1, count_2, count_3, count_4 = count_for_confusion_matrix(y_test, y_pred, 
-                                                                        count_1, count_2, count_3, count_4)
+		count_1, count_2, count_3, count_4 = count_for_confusion_matrix(y_test, y_pred, 
+																		count_1, count_2, count_3, count_4)
 
-    print ('False Predicted: %s' % diff_count)
-    return count_1, count_2, count_3, count_4
+	print ('False Predicted: %s' % diff_count)
+	return count_1, count_2, count_3, count_4
 
 
 if __name__  == "__main__":	
